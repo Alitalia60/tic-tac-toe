a7248f3d5763fedd9c0f71fdb1b613670349fb1b
Andrew Litvinov <lita.study@bk.ru>
2022-02-18 21:54:26 +0300

    feat: all point of task done

--- tic-tak-toe/script.js
+++ tic-tak-toe/script.js
@@ -1,52 +1,53 @@
 let gameStatus = 'ready';
 
 const messages = {
-    x_win: 'X wins',
-    o_win: 'O wins',
-    noWinner: 'NO WINNER',
-    ready: "READY",
-    paused: "GAME PAUSED",
-    over: "GAME OVER",
-}
-let step = 0;
-let move = "0";
-const cells = {
-    a1: "",
-    a2: "",
-    a3: "",
-    b1: "",
-    b2: "",
-    b3: "",
-    c1: "",
-    c2: "",
-    c3: "",
+    x_win: '"X" wins',
+    o_win: '"O" wins',
+    draw: 'DRAW',
+    ready: 'READY',
+    paused: 'GAME PAUSED',
+    over: 'GAME OVER',
 };
+let step = 0;
+let
+    whoseMove = '0';
+const cells = {
+    a1: '',
+    a2: '',
+    a3: '',
+    b1: '',
+    b2: '',
+    b3: '',
+    c1: '',
+    c2: '',
+    c3: '',
+};
+const colorCellX = 'yellow';
+const colorCellO = 'cyan';
 
+let countDraw = 0;
 let countWinsX = 0;
 let countWinsO = 0;
+const completedMoves = [];
 
-document.addEventListener("DOMContentLoaded", init);
-const backSound = document.createElement("audio");
-const clickSound = document.createElement("audio");
+document.addEventListener('DOMContentLoaded', init);
+const backSound = document.createElement('audio');
+const clickSound = document.createElement('audio');
 
 let soundMute = false;
 let musicMute = false;
 
-const field = document.getElementById("field");
-
+const field = document.getElementById('field');
 
 const switchMusicMute = document.querySelector('.music_mute');
-switchMusicMute.addEventListener('click', toggleVolume)
+switchMusicMute.addEventListener('click', toggleVolume);
 
 const switchSoundMute = document.querySelector('.sound_mute');
-switchSoundMute.addEventListener('click', toggleVolume)
+switchSoundMute.addEventListener('click', toggleVolume);
 
-
-const gameOverBox = document.querySelector(".game_over_box");
-gameOverBox.addEventListener('click', pauseGame);
+const gameOverBox = document.querySelector('.game_over_box');
 
 const btnPlay = document.querySelector('.btn_start_game');
-
 
 //=================================================
 function toggleVolume(ev) {
@@ -59,7 +60,7 @@ function toggleVolume(ev) {
         case switchSoundMute:
             soundMute = !soundMute;
             switchSoundMute.classList.toggle('switch_off');
-            playSounds()
+            playSounds();
             break;
         default:
             break;
@@ -68,43 +69,58 @@ function toggleVolume(ev) {
 
 //=================================================
 function init() {
-
-    btnPlay.addEventListener('click', playGame)
-    backSound.src = "./assets/audio/preview.mp3";
+    btnPlay.addEventListener('click', changeAction);
+    backSound.src = './assets/audio/preview.mp3';
     backSound.volume = 0.1;
-    clickSound.src = "/tic-tak-toe/assets/audio/click.wav";
+    clickSound.src = '/tic-tak-toe/assets/audio/click.wav';
+    showInfoBox([messages.ready, 'white'])
 }
 
 //=================================================
-function playGame() {
-    if (gameStatus == 'ready') {
-        gameStatus = 'running';
-
-        clearField();
-        playSounds()
-        playMusic()
-        step = 0;
-        showStatusBox();
-        field.addEventListener("click", putLabel);
-    } else if (gameStatus == 'paused') {
-
+function changeAction() {
+    if (gameStatus != 'running') {
+        console.log('gameStatus1=', gameStatus);
+        gameStatus = 'running'
+        hideInfoBox()
+        playGame()
+    } else if (gameStatus == 'running') {
+        console.log('gameStatus2=', gameStatus);
+        gameStatus = 'paused'
+        showInfoBox([messages.paused, 'green'])
+        pauseGame()
     }
-
-    hideInfoBox('')
+    console.log('gameStatus3=', gameStatus);
 }
 
+//=================================================
+function playGame(ev) {
+    field.addEventListener('click', putLabel);
+    if (gameStatus != 'paused') {
+        // gameStatus = 'running';
+        completedMoves.length = 0;
+        clearField();
+        playSounds();
+        playMusic();
+        step = 0;
+        showStatusBox();
+    } else {
+        hideInfoBox('');
+    }
+}
+
+//=================================================
 function playSounds() {
     if (!soundMute) {
         clickSound.play();
     }
 }
 
+//=================================================
 function playMusic() {
     if (!musicMute) {
         backSound.play();
     } else {
         backSound.pause();
-
     }
 }
 
@@ -114,38 +130,44 @@ function clearField() {
 
     for (const item of document.querySelectorAll('.cell')) {
         item.style.backgroundColor = null;
-        item.classList.remove('cell_krug', 'cell_krest')
+        item.classList.remove('cell_krug', 'cell_krest');
     }
     for (let item in cells) {
-        cells[item] = "";
+        cells[item] = '';
     }
 }
 
 //===================================
-function putLabel(e) {
+function putLabel(ev) {
     if (gameStatus == 'paused') {
-        return
+        return;
     }
-    if (e.target.classList.contains("cell")) {
-        playSounds()
-        step++;
-        let cell = e.target;
-        if (cell.textContent == "") {
-            if (move == "0") {
-                cell.style.backgroundColor = "green";
-                cell.classList.add('cell_krug')
-                cells[cell.id] = "0";
-                cell.style.transform = "rotateX(180deg)";
-            } else {
-                cell.style.backgroundColor = "red";
-                cell.classList.add('cell_krest')
-                cells[cell.id] = "X";
-                cell.style.transform = "rotateZ(180deg)";
-                // cell.style.transform = "rotateY(180deg)";
-            }
-            move = move == "0" ? "X" : "0";
+    if (ev.target.classList.contains('cell')) {
+        let cell = ev.target;
+        if (completedMoves.includes(cell.id)) {
+            return;
         }
-
+        completedMoves.push(cell.id);
+        // console.log(completedMoves.length);
+        playSounds();
+        step++;
+        if (cell.textContent == '') {
+            if (
+                whoseMove == '0') {
+                cell.style.backgroundColor = 'cyan';
+                cell.classList.add('cell_krug');
+                cell.style.transform = 'rotateX(180deg)';
+                cells[cell.id] = '0';
+            } else {
+                cell.style.backgroundColor = 'yellow';
+                cell.classList.add('cell_krest');
+                cell.style.transform = 'rotateZ(180deg)';
+                cells[cell.id] = 'X';
+                // cell.style.transform = 'rotateY(180deg)';
+            }
+            whoseMove =
+                whoseMove == '0' ? 'X' : '0';
+        }
         showStatusBox();
         checkWinner();
     }
@@ -153,81 +175,96 @@ function putLabel(e) {
 
 //=================================================
 function checkWinner() {
-    if (step >= 9) {
-        gameOver(messages.noWinner);
+    if (
+        cells.a1 + cells.b2 + cells.c3 == 'XXX' ||
+        cells.a3 + cells.b2 + cells.c1 == 'XXX' ||
+        cells.a1 + cells.a2 + cells.a3 == 'XXX' ||
+        cells.b1 + cells.b2 + cells.b3 == 'XXX' ||
+        cells.c1 + cells.c2 + cells.c3 == 'XXX' ||
+        cells.a1 + cells.b1 + cells.b3 == 'XXX' ||
+        cells.a2 + cells.b2 + cells.c2 == 'XXX' ||
+        cells.a3 + cells.b3 + cells.c3 == 'XXX'
+    ) {
+        countWinsX++;
+        gameOver();
+        showInfoBox([messages.over, 'red'], [messages.x_win, 'yellow']);
         return;
-    } else {
-
-
-        if (
-            cells.a1 + cells.b2 + cells.c3 == "XXX" ||
-            cells.a3 + cells.b2 + cells.c1 == "XXX" ||
-            cells.a1 + cells.a2 + cells.a3 == "XXX" ||
-            cells.b1 + cells.b2 + cells.b3 == "XXX" ||
-            cells.c1 + cells.c2 + cells.c3 == "XXX" ||
-            cells.a1 + cells.b1 + cells.b3 == "XXX" ||
-            cells.a2 + cells.b2 + cells.c2 == "XXX" ||
-            cells.a3 + cells.b3 + cells.c3 == "XXX"
-        ) {
-            countWinsX++;
-            gameOver(messages.x_win);
-            return;
-        };
-        if (
-            cells.a1 + cells.b2 + cells.c3 == "000" ||
-            cells.a3 + cells.b2 + cells.c1 == "000" ||
-            cells.a1 + cells.a2 + cells.a3 == "000" ||
-            cells.b1 + cells.b2 + cells.b3 == "000" ||
-            cells.c1 + cells.c2 + cells.c3 == "000" ||
-            cells.a1 + cells.b1 + cells.b3 == "000" ||
-            cells.a2 + cells.b2 + cells.c2 == "000" ||
-            cells.a3 + cells.b3 + cells.c3 == "000"
-        ) {
-            countWinsO++;
-            gameOver(messages.o_win);
-            return;
-        }
+    }
+    if (
+        cells.a1 + cells.b2 + cells.c3 == '000' ||
+        cells.a3 + cells.b2 + cells.c1 == '000' ||
+        cells.a1 + cells.a2 + cells.a3 == '000' ||
+        cells.b1 + cells.b2 + cells.b3 == '000' ||
+        cells.c1 + cells.c2 + cells.c3 == '000' ||
+        cells.a1 + cells.b1 + cells.b3 == '000' ||
+        cells.a2 + cells.b2 + cells.c2 == '000' ||
+        cells.a3 + cells.b3 + cells.c3 == '000'
+    ) {
+        countWinsO++;
+        gameOver();
+        showInfoBox([messages.over, 'red'], [messages.o_win, 'yellow']);
+        return;
+    }
+    if (completedMoves.length >= 9) {
+        countDraw++;
+        gameOver();
+        showInfoBox([messages.over, 'red'], [messages.draw, 'yellow']);
+        return;
     }
 }
 
 //=================================================
-function gameOver(message) {
+function gameOver() {
     gameStatus = 'finished';
-    field.removeEventListener("click", putLabel);
+    field.removeEventListener('click', putLabel);
     showStatusBox();
-    showInfoBox(message)
+    btnPlay.textContent = 'PRESS TO RESTART';
 }
 
-function showInfoBox(message) {
-
-    gameOverBox.style.top = "150px";
-    const h2 = document.querySelector(".game_status");
-    h2.textContent = message
+//=================================================
+function showInfoBox(mes1, mes2) {
+    gameOverBox.style.top = '150px';
     gameOverBox.style.cursor = 'auto';
+
+    btnPlay.style.bottom = '70px'
+
+    const h2_line_1 = document.querySelector('.status_line_1');
+    h2_line_1.textContent = mes1[0];
+    h2_line_1.style.color = mes1[1];
+
+    const h2_line_2 = document.querySelector('.status_line_2');
+    if (mes2) {
+        h2_line_2.textContent = mes2[0];
+        h2_line_2.style.color = mes2[1];
+    } else {
+        h2_line_2.textContent = '';
+        h2_line_2.style.color = 'none';
+
+    }
 }
 
-function hideInfoBox(message) {
-    // const gameOverBox = document.querySelector(".game_over_box");
+//=================================================
+function hideInfoBox() {
     gameOverBox.style.cursor = 'pointer';
-    gameOverBox.style.top = "-400px";
-    const h2 = document.querySelector(".game_status");
-    h2.textContent = message
+    gameOverBox.style.top = '-370px';
+    btnPlay.style.bottom = '-10px'
+    btnPlay.textContent = 'PRESS TO PAUSE'
 
 }
 
+//=================================================
 function showStatusBox() {
     document.querySelector('.score_step').textContent = 'Moves:' + step;
+    document.querySelector('.score_draw').textContent = 'Draw:' + countDraw;
     document.querySelector('.score_x_wins').textContent = 'X wins:' + countWinsX;
     document.querySelector('.score_o_wins').textContent = 'O wins:' + countWinsO;
 }
 
-function pauseGame() {
-    gameStatus = (gameStatus == 'paused') ? 'running' : 'paused';
-    if (gameStatus == 'running') {
-        showInfoBox(messages.paused);
-        btnPlay.textContent = 'PRESS TO CONTINUE'
-    } else {
-        hideInfoBox()
-    }
-
+//=================================================
+function pauseGame(ev) {
+    musicMute = true;
+    playMusic()
+    field.removeEventListener('click', putLabel)
+    showInfoBox([messages.paused, 'yellow']);
+    btnPlay.textContent = 'PRESS TO CONTINUE';
 }
